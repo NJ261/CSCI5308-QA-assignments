7035364c925c34bf904fe3182a69f636c1c0c814
Nirav Jadeja <nirav9033@gmail.com>
2018-06-01 08:14:47 -0300

    main index updated

--- A1/A1.py
+++ A1/A1.py
@@ -1,4 +1,7 @@
 import xml.etree.ElementTree as ET
+import security
+import part_manager
+
 
 tree = ET.parse('in_order.xml')
 
@@ -40,68 +43,16 @@ for order in order_items.findall('item'):
 #if None in order_quantity or order_part_number or delivery_add_tag or dealer_tag or main_tag:
 #   print "************************   Invalid input XML   ************************"
         
-        
 
-def validate_dealer(dealerId, dealerKey):
-    temp = "Invalid Input XML Response"
-    if dealerId is None:
-        print temp                  # checking DEALER ID is not null
-    elif dealerKey is None:
-        print temp                  # checking DEALER ACCESS KEY is not null
-    else:
-        temp = "Dealer Authenticated"
-        pass
-    return temp
+sec = security.Security(dealer_tag[0], dealer_tag[1])
+print(sec.authenticate())
 
-def validate_parts(part_number, quantity):
-    temp = []
-    temp_msg = ""
-    temp_part_number = [part_number]
-    temp_quantity = [quantity]
-    
-    for i in range(0, len(temp_part_number)):
-        
-        if temp_part_number[i] is None:
-            temp_msg = "Invalid Input XML Response: Error in Part number"
-            print temp_msg
-            break
-        
-        else:
-            temp.append(int(part_number[i]))
-            temp_msg = "Part Number and Quantity are good."
-            
-            if temp_quantity[i] is None:
-                temp_msg = "Invalid Input XML Response: Error in Quantity"
-                print temp_msg
-                break
-                
-            else:
-                temp_quantity = int(quantity[i])
-                if temp_quantity < 1:
-                    temp_msg = "Invalid Input XML Response: Error in Quantity"
-                    print temp_msg 
-                    break
-                else:
-                    temp_msg = "Part Number and Quantity are good."
-                    break
-            
-    return temp_msg
+part_manage = part_manager.Part_Manager(order_part_number, order_quantity)
+print(part_manage.validate_parts())
 
-def validate_delivery(delivery_details):
-    temp_msg = ""
-    for elem in range(0, len(delivery_details)):
-        if delivery_details[elem] is None:
-            temp_msg = "Invalid Input XML Response: Error in Delivery Details"
-            print temp_msg
-        else:
-            temp_msg = "Delivery Details are good"
-            
-    return temp_msg
-
-        
-validate_dealer(dealer_tag[0], dealer_tag[1])    
-validate_parts(order_part_number, order_quantity)
-validate_delivery(delivery_add_tag)
+delivery_details = part_manager.delivery_address(delivery_add_tag[0], delivery_add_tag[1],
+                                                 delivery_add_tag[2],delivery_add_tag[3],delivery_add_tag[4])
+delivery_details.validate_delivery()  
 
 
 """ REFERENCES:
