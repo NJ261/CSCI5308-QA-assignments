8904844bf56ec27d6d38634fb44fad0c35bc7cf7
Nirav Jadeja <nirav9033@gmail.com>
2018-06-01 07:12:35 -0300

    update in mock data

--- A1/mock_data.py
+++ A1/mock_data.py
@@ -1,6 +1,8 @@
-dealer_list = ["", "", "", "", ""]
+dealer_list = ["XXX-1234-ABCD-1234", "XXX-1111-ABCD-1111", 
+               "XXX-2222-ABCD-2222", "XXX-3333-ABCD-3333", "XXX-4444-ABCD-4444"]
 
-dealer_key = ["","","","",""]
+dealer_key = ["kkklas8882kk23nllfjj88290","kkklas8882kk23nllfjj88291",
+              "kkklas8882kk23nllfjj88292","kkklas8882kk23nllfjj88293","kkklas8882kk23nllfjj88294"]
 
 part_number = ["1234","1111","2222","3333","4444"]
 
