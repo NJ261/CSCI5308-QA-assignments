bcec5668842c0caaec9e0564fd8a4faffed7790a
Nirav Jadeja <nirav9033@gmail.com>
2018-06-01 14:39:25 -0300

    refactoring done according to test

--- A1/part_manager.py
+++ A1/part_manager.py
@@ -6,14 +6,16 @@ class Part_Manager:
         self.part_number = part_number
         self.quantity = quantity
         
+    # validating part number and quantity    
     def validate_parts(self):
         temp = []
         temp_msg = ""
         temp_part_number = [self.part_number]
         temp_quantity = [self.quantity]
-        
+    
         for i in range(0, len(temp_part_number)):
             
+            # checking part number for blank input
             if temp_part_number[i] is None:
                 temp_msg = "Invalid Input XML Response: Error in Part number"
                 print temp_msg
@@ -21,8 +23,8 @@ class Part_Manager:
             
             else:
                 temp.append(int(self.part_number[i]))
-                temp_msg = "Part Number and Quantity are good."
-                
+                                
+                # checking quantity for blank response
                 if temp_quantity[i] is None:
                     temp_msg = "Invalid Input XML Response: Error in Quantity"
                     print temp_msg
@@ -30,32 +32,37 @@ class Part_Manager:
                     
                 else:
                     temp_quantity = int(self.quantity[i])
+                    
+                    # checking quantity for 0 or some negative value
                     if temp_quantity < 1:
                         temp_msg = "Invalid Input XML Response: Error in Quantity"
                         print temp_msg 
-                        break
-                    else:
-                        temp_msg = "Part Number and Quantity are good."
-                        break
+                        break                        
+                
+                temp_msg = "Part Number and Quantity are good."
                 
         return temp_msg
     
-        
-    
-        
+    # part number checking with mock data, returning value with item status i.e success, invalid part, out of stock
     def part_number_checking(self):
         
         temp_length = len(mock_data.part_number)
-        
-        for i in range(0, temp_length):
-            if self.part_number in mock_data.part_number[i]:
-                msg = mock_data.part_number_msg[i]
+        temp_list = self.part_number
+        msg = []
+
+        for i in range (0, len(self.part_number)):
+            if temp_list[i] in mock_data.part_number:
                 
+                for j in range (0, temp_length):    
+                    if temp_list[i] in mock_data.part_number[j]:
+                        msg.append(mock_data.part_number_msg[j])
+
             else:
-                msg = "invalid part"
-                
-            return msg
+                msg.append("Invalid Part")
+        return msg
         
+    
+# for verifying details whether they are not empty
 class delivery_address:
     
     def __init__(self, name, address, city, province, postalCode):
@@ -74,6 +81,7 @@ class delivery_address:
             if temp_delivery_details[elem] is None:
                 temp_msg = "Invalid Input XML Response: Error in Delivery Details"
                 print temp_msg
+                break
             else:
                 temp_msg = "Delivery Details are good"
                 
