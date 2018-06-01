9084a1572d08056dcdc17e9241ba359c0b19f3bc
Nirav Jadeja <nirav9033@gmail.com>
2018-06-01 14:38:10 -0300

    Refactoring and writing output XML file done

--- A1/A1.py
+++ A1/A1.py
@@ -2,9 +2,7 @@ import xml.etree.ElementTree as ET
 import security
 import part_manager
 
-
 tree = ET.parse('in_order.xml')
-
 root = tree.getroot()
 
 main_tag = []
@@ -13,7 +11,7 @@ order_part_number = []
 order_quantity = []
 delivery_add_tag = []
 
-# ---------- input XML reading ----------
+# ---------- input XML reading -------------
 for child in root:
     main_tag.append(child.tag)
     
@@ -40,50 +38,95 @@ for order in order_items.findall('item'):
         order_quantity.append(quantity.text)
         order_part_number.append(part_number.text)
 
-#if None in order_quantity or order_part_number or delivery_add_tag or dealer_tag or main_tag:
-#   print "************************   Invalid input XML   ************************"
+# ---------- Writing output XML -------------
         
+# writing XML: dealer not authorized XML
+def not_authorized_XML():
+    
+    order = ET.Element("order")
+    result = ET.SubElement(order, "result").text = "failure"
+    error = ET.SubElement(order, "error"). text = "Not authorized"
+    
+    tree = ET.ElementTree(order)
+    tree.write("output.xml")
+    
+# writing XML: invalid input XML response
+def invalid_response_XML(error_msg):
+    
+    order = ET.Element("order")
+    result = ET.SubElement(order, "result").text = "failure"
+    error = ET.SubElement(order, "error"). text = "Invalid Order"
+    errormessage = ET.SubElement(order, "errormessage"). text = error_msg
+    
+    tree = ET.ElementTree(order)
+    tree.write("output.xml")
+    
+# writing XML: authorized XML response
+def authorized_response_XML(error_response):
+    
+    temp_part_length = len(order_part_number)
+    
+    order = ET.Element("order")
+    orderitems = ET.SubElement(order, "orderitems")
+    
+    for i in range(0, temp_part_length):
+        item = ET.SubElement(orderitems, "item")
+        partnumber = ET.SubElement(item, "partnumber").text = order_part_number[i]
+        quantity = ET.SubElement(item, "quantity").text = order_quantity[i]
+
+        if error_response[i] == "success":
+            result = ET.SubElement(item, "result").text = "success"
+            errormessage = ET.SubElement(item, "errormessage").text = ""
+        else:
+            result = ET.SubElement(item, "result").text = "failure"
+            errormessage = ET.SubElement(item, "errormessage").text = error_response[i]
+                  
+    tree = ET.ElementTree(order)
+    tree.write("output.xml")
+            
+
+
 sec = security.Security(dealer_tag[0], dealer_tag[1])
 part_manage = part_manager.Part_Manager(order_part_number, order_quantity)
 delivery_details = part_manager.delivery_address(delivery_add_tag[0], delivery_add_tag[1],
                                                  delivery_add_tag[2],delivery_add_tag[3],delivery_add_tag[4])
 dealer_validate = sec.validate_dealer()
 
+# validating dealer id and access key field
 if len(dealer_validate) < 25:
     parts_validation = part_manage.validate_parts()
     
+    # validating part number and quantity field
     if len(parts_validation) < 40:
         delivery_details_validation = delivery_details.validate_delivery()
         
-        if len(delivery_details_validation) < 40:
+        # validating delivery details fields such as name, address, city, province, postal code
+        if len(delivery_details_validation) < 30:
             dealer_auth = sec.authenticate()
             
+            # Dealer authentication with provided dealer id and access key
             if len(dealer_auth) < 21:
                 part_check = part_manage.part_number_checking()
                 
                 if len(part_check) < 8:
-                    print "here to construct valid XML response. ***********"
-                    
-                else:
-                    print "part check error XML response i.e out of stock, no longer manufactured ***********"
+                    # writing successful XML response 
+                    authorized_response_XML(part_check)
                     
             else:
-                print "dealer not authozed XML Response ************"
+                # writing Fault XML Response: Dealer is not authorized
+                not_authorized_XML()    
                 
         else:
-            print "delivery details XML response *********"
+            # writing Fault XML response: Delivery details
+            invalid_response_XML(delivery_details_validation)   
             
     else:
-        print "part validation XML response ********"
-
-    
+        # writing Fault XML response: Part number and quantity
+        invalid_response_XML(parts_validation)      
+        
 else:
-    print dealer_validate
-
-
-print()
-
-  
+    # writing Fault XML Response: Dealer ID or Access key
+    invalid_response_XML(dealer_validate)       
 
 
 """ REFERENCES:
