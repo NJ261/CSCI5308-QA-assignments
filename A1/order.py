6f43127485788c1b8b2fb88b57eb94606ab287ef
Nirav Jadeja <nirav9033@gmail.com>
2018-06-02 20:10:18 -0300

    order file for taking input xml

---
+++ A1/order.py
@@ -0,0 +1,14 @@
+import A1
+
+file_name = raw_input("Enter the file Name (i.e: input.xml):  ")
+
+A1.input_function(file_name)
+
+""" REFERENCES:
+    1. https://docs.python.org/2/library/xml.etree.elementtree.html
+    2. https://stackoverflow.com/questions/49259985/addin-multiple-sub-elements-with-same-tag-to-en-xml-tree-with-python-elementtree
+    3. https://stackoverflow.com/questions/3605680/creating-a-simple-xml-file-using-python
+    4. https://stackoverflow.com/questions/45736496/unittest-assert-a-set-of-items-of-a-list-are-or-not-contained-in-another-lis
+    5. https://stackoverflow.com/questions/36372832/python-valueerror-invalid-literal-for-int-with-base-10
+    6. https://stackoverflow.com/questions/11282401/can-i-fake-mock-the-type-of-my-mock-objects-in-python-unittests
+    """
