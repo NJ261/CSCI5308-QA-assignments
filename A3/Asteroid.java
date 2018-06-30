a9974fbfb0059bcd33a81b789e832f7d71190d3f
Nirav Jadeja <nirav9033@gmail.com>
2018-06-29 23:53:05 -0300

    update in asteroid

--- A3/Asteroid.java
+++ A3/Asteroid.java
@@ -12,19 +12,9 @@ public class Asteroid extends BoardComponent
 	@Override
 	public void Operation()
 	{
-		// The operation performed by Asteroid objects is to fall from the sky
-		// one level at a time, when they hit the ground (height == 0) they impact
-		// and destroy whatever buildings are in the square!
 		height -= 1;
 		if (0 == height)
 		{
-			// When an Asteroid impacts the ground it needs to send an event to the
-			// observer to tell it that it impacted the ground in the square it belongs
-			// to.
-			// <-- Send event to observer.
-			
-			// It should then remove itself from its parent, it no longer exists in the
-			// hierarchy and should not receive any more operations.
 			parent.Remove(this);		
 		}
 	}
